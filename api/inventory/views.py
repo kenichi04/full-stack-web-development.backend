cbb69fc34c02903e51d6a4fe5d405b21b5297fb4
kenichi04 <slow_life_1407@yahoo.co.jp>
2024-06-30 07:57:31 +0000

    6-5-3 add product get api

--- api/inventory/views.py
+++ api/inventory/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Product
+from .serializers import ProductSerializer
+from rest_framework import status
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+class ProductView(APIView):
+    """
+    商品操作に関する関数
+    """
+    def get(self, request, format=None):
+        """
+        商品一覧の取得
+        """
+        queryset = Product.objects.all()
+        serializer = ProductSerializer(queryset, many=True)
+        return Response(serializer.data, status.HTTP_200_OK)
+
+class ProductModelViewSet(ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
